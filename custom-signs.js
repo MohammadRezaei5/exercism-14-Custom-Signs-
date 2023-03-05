167e66d3a7ada5affba249348ab70cf3a9a2d520
mhmmd <mhmmd.rezaei4@gmail.com>
2023-03-05 10:12:18 +0330

    Function: buildBirthdaySign

--- custom-signs.js
+++ custom-signs.js
@@ -1,5 +1,15 @@
 function buildSign(occasion, name) {
-    return `Happy ${occasion} ${name}!`;
-  }
-  
-  // console.log(buildSign("Birthday", "Rob"));
+  return `Happy ${occasion} ${name}!`;
+}
+
+// console.log(buildSign("Birthday", "Rob"));
+
+///---///
+
+function buildBirthdaySign(age) {
+  return `Happy Birthday! What a ${
+    age >= 50 ? "mature" : "young"
+  } fellow you are.`;
+}
+
+// console.log(buildBirthdaySign(45));
